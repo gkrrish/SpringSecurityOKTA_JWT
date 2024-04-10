670fb6713e2df5dcc0dae9d4603e61a960b9e2fa
Krishna <gkrrish.11@gmail.com>
2024-04-10 12:16:09 +0530

    initial commit

---
+++ src/main/java/com/springsecurityokta/SpringSecurityOktaJwtApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurityokta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOktaJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOktaJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecurityokta/SpringSecurityOktaJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurityokta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOktaJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
