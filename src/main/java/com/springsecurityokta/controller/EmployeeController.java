88e2b9f9afe6264630a7e8c5d604fd9e54f534bb
Krishna <gkrrish.11@gmail.com>
2024-04-17 15:07:06 +0530

    basic security things added, but still have some issue

--- src/main/java/com/springsecurityokta/controller/EmployeeController.java
+++ src/main/java/com/springsecurityokta/controller/EmployeeController.java
@@ -1,14 +1,16 @@
 package com.springsecurityokta.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
+	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/test")
+//	@RolesAllowed("USER")//have limited capabilities, use the PreAuthorize
 	public String get() {
 		return "test";
 	}
-
 }
--- src/main/java/com/springsecurityokta/loginAuthentication/config/SecurityConfig.java
+++ src/main/java/com/springsecurityokta/loginAuthentication/config/SecurityConfig.java
@@ -2,18 +2,36 @@ package com.springsecurityokta.loginAuthentication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
-	/*
-	 * @Bean public SecurityFilterChain filterChain(HttpSecurity http) throws
-	 * Exception { http.addFilter(null);
-	 * 
-	 * 
-	 * 
-	 * return http.build(); }
-	 */
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+					.csrf(Customizer.withDefaults())
+					.authorizeHttpRequests(authorize->
+								authorize
+								.anyRequest()
+								.authenticated())
+					.httpBasic(Customizer.withDefaults())
+					.formLogin(Customizer.withDefaults());
+		
+		 return httpSecurity.build();
+
+	}
+
+	@Bean
+	 PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
